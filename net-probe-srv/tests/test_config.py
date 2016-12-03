018df22c1197083968c6359ded40a2c9c74df550
Alex Chauvin <ach@meta-x.org>
2016-12-03 22:42:06 +0100

    template for jobs and tests

--- net-probe-srv/tests/test_config.py
+++ net-probe-srv/tests/test_config.py
@@ -1,6 +1,6 @@
 # -*- Mode: Python; python-indent-offset: 4 -*-
 #
-# Time-stamp: <2016-11-12 16:54:50 alex>
+# Time-stamp: <2016-12-03 22:40:57 alex>
 #
 
 import sys
@@ -209,6 +209,236 @@ def test_active_flag():
         assert False, "bad active status on reload"
 
 # ---------------------------------------------
+def test_template01():
+    """ apply a template to a host for job
+    """
+    global app
+    global lDB
+    lDB.cleanDB()
+
+    dConf = {
+        "template" : 
+        [
+            { 
+                "name": "T01",
+                "jobs" : [
+                    { 
+                        "active": "True",
+                        "job" : "health",
+                        "freq" : 15,
+                        "version" : 1,
+                        "data" : {}
+                    }
+                ]
+            }
+        ],
+
+        "output" :  [ { "engine": "debug",
+                        "parameters" : [],
+                        "active" : "False"    }  ],
+        "probe" : [
+            { "id" : "temp01",
+              "probename" : "temp01",
+              "template" : [
+                  "T01"
+              ]
+          }
+        ]
+    }
+
+    sConf = string.replace(str(dConf), "'", '"')
+
+    try:
+        f = file("test_config.conf", 'w')
+    except IOError:
+        logging.error("accessing config file {}".format(sFile))
+        return False
+        
+    f.write(sConf)
+    f.close()
+
+    global conf
+    conf.loadFile('test_config.conf')
+
+    c = app.test_client()
+
+    # register the probe
+    rv = c.post("/discover", data=dict(hostId="temp01",ipv4="127.1.0.5",ipv6="::1",version="0.0"))
+    j = json.loads(rv.data)
+    if j['answer'] != "OK":
+        assert False, "should have found this host"
+
+    # get the configuration
+    rv = c.post("/myjobs", data=dict(uid=str(j['uid'])))
+    j = json.loads(rv.data)
+    if j['answer'] != "OK":
+        assert False, "should have found this host"
+
+    job = j['jobs'][0]
+
+    if not job.__contains__('active'):
+        assert False, "active not present"
+
+    if job['active'] != "True":
+        assert False, "active present but not True"
+
+    if job['job'] != "health":
+        assert False, "job should be health"
+
+    if job['freq'] != 15:
+        assert False, "job frequency should be 15"
+
+    if job['id'] != 1000:
+        assert False, "job id should be 1000"
+
+# ---------------------------------------------
+def test_template02():
+    """ multiple templates, 3 hosts with one, another or both
+    """
+    global app
+    global lDB
+    lDB.cleanDB()
+
+    dConf = {
+        "template" : 
+        [
+            { 
+                "name": "T01",
+                "jobs" : [
+                    { 
+                        "active": "True",
+                        "job" : "health",
+                        "freq" : 101,
+                        "version" : 1,
+                        "data" : {}
+                    }
+                ]
+            },
+            { 
+                "name": "T02",
+                "jobs" : [
+                    { 
+                        "active": "True",
+                        "job" : "health",
+                        "freq" : 102,
+                        "version" : 1,
+                        "data" : {}
+                    }
+                ]
+            }
+        ],
+
+        "output" :  [ { "engine": "debug",
+                        "parameters" : [],
+                        "active" : "False"    }  ],
+        "probe" : [
+            { "id" : "temp01",
+              "probename" : "temp01",
+              "template" : [
+                  "T01"
+              ]
+          }, 
+            {
+                "id" : "temp02",
+                "probename" : "temp02",
+                "template" : [
+                    "T02"
+                ]
+            },
+            
+            {
+                "id" : "temp03",
+                "probename" : "temp03",
+                "template" : [
+                    "T01", "T02"
+                ]
+            }
+        ]
+    }
+
+    sConf = string.replace(str(dConf), "'", '"')
+
+    try:
+        f = file("test_config.conf", 'w')
+    except IOError:
+        logging.error("accessing config file {}".format(sFile))
+        return False
+        
+    f.write(sConf)
+    f.close()
+
+    global conf
+    conf.loadFile('test_config.conf')
+
+    c = app.test_client()
+
+    # register the probe
+    rv = c.post("/discover", data=dict(hostId="temp01",ipv4="127.1.0.6",ipv6="::1",version="0.0"))
+    j = json.loads(rv.data)
+    if j['answer'] != "OK":
+        assert False, "should have found this host"
+
+    # get the configuration
+    rv = c.post("/myjobs", data=dict(uid=str(j['uid'])))
+    j = json.loads(rv.data)
+
+    if j['answer'] != "OK":
+        assert False, "should have found this host"
+
+    job = j['jobs'][0]
+
+    if job['freq'] != 101:
+        assert False, "job for temp01 should have freq 101"
+
+    if job['id'] != 1000:
+        assert False, "job for temp01 id != 1000"
+
+    # register the probe
+    rv = c.post("/discover", data=dict(hostId="temp02",ipv4="127.1.0.7",ipv6="::1",version="0.0"))
+    j = json.loads(rv.data)
+    if j['answer'] != "OK":
+        assert False, "should have found this host"
+
+    # get the configuration
+    rv = c.post("/myjobs", data=dict(uid=str(j['uid'])))
+    j = json.loads(rv.data)
+    if j['answer'] != "OK":
+        assert False, "should have found this host"
+
+    job = j['jobs'][0]
+
+    if job['freq'] != 102:
+        assert False, "job for temp02 should have freq 102"
+
+    if job['id'] != 1001:
+        assert False, "job for temp02 id != 1001"
+
+    # register the probe
+    rv = c.post("/discover", data=dict(hostId="temp03",ipv4="127.1.0.8",ipv6="::1",version="0.0"))
+    j = json.loads(rv.data)
+    if j['answer'] != "OK":
+        assert False, "should have found this host"
+
+    # get the configuration
+    rv = c.post("/myjobs", data=dict(uid=str(j['uid'])))
+    j = json.loads(rv.data)
+    if j['answer'] != "OK":
+        assert False, "should have found this host"
+
+    job = j['jobs'][0]
+
+    if j['jobs'][0]['freq'] != 101:
+        assert False, "job #1 for temp03 should have freq 101"
+
+    if j['jobs'][1]['freq'] != 102:
+        assert False, "job #2 for temp03 should have freq 101"
+
+    if j['jobs'][0]['id'] != 1002:
+        assert False, "job #1 for temp03 id != 1002"
+    if j['jobs'][1]['id'] != 1003:
+        assert False, "job #2 for temp03 id != 1003"
+
+# ---------------------------------------------
 def all(b=True):
     if b:
         test_addHost()
@@ -216,8 +446,10 @@ def all(b=True):
         test_checkHost()
         test_duplicateProbeName()
         test_getConf()
+        test_active_flag()
+        test_template01()
 
-    test_active_flag()
+    test_template02()
 
 # ---------------------------------------------
 if __name__ == '__main__':
