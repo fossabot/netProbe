99ff9b59bba5614d2f780860e013b9396d5684d2
Alex Chauvin <ach@meta-x.org>
2017-04-09 14:15:56 +0200

    new probe database
    redis and test

---
+++ py-net-probe/database/dbTest.py
@@ -0,0 +1,103 @@
+# -*- Mode: Python; python-indent-offset: 4 -*-
+#
+# Time-stamp: <2017-04-09 11:32:27 alex>
+#
+#
+# --------------------------------------------------------------------
+# PiProbe
+# Copyright (C) 2016-2017  Alexandre Chauvin Hameau <ach@meta-x.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# --------------------------------------------------------------------
+
+"""
+ probe database for configuration and data
+"""
+
+import logging
+import time
+import json
+import sys
+
+# import pprint
+
+class dbTest(object):
+    """
+    database class for testing only
+    """
+
+    def __init__(self, host=None):
+        """
+        constructor
+        """
+        self.db = None
+
+    def connect(self, host=None):
+        """
+        """
+
+        self.db = True
+
+    def cleanJob(self, jobName):
+        """suppress the list from the database
+
+        """
+        logging.info("delete {}".format(jobName))
+
+    def addJob(self, jobName, job):
+        """add a job in the job list
+
+        """
+        logging.info("add job {} {}".format(jobName,json.dumps(job)))
+
+    def getJobs(self, jobName):
+        """extracts all jobs and return an array
+
+        """
+        logging.info("getJobs")
+
+        s = sys.stdin.read()
+        a = []
+
+        a=json.loads(s)
+
+        return a
+
+    def dumpJob(self, jobName):
+        """dump the content of the db for jobname, return a generator
+
+        """
+        logging.info("dumpJob")
+
+    def pushResult(self, result):
+        """add a result in the queue for the main process
+        result is a dict
+
+        """
+        if not isinstance(result, dict):
+            raise Exception("pushResult not provided a dict")
+
+        logging.info("push result {}".format(result))
+
+    def popResult(self):
+        """pop a result from the queue
+        return None if nothing in the queue
+        """
+        logging.info("pop result")
+
+    def lenResultQueue(self):
+        """get queue size
+        """
+        logging.info("lenResultQueue")
+        return 0
